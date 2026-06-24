fc7f8a04c53190430a82617d5d9f6fad08fb92c0
zufa1984 <abdulqodirkhamidov@gmail.com>
2026-06-24 16:18:10 +0500

    change app name and logo

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Readio',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: const SplashScreen(),
